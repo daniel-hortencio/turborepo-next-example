d745cf7e080d685910cca7ffce906e87d3300a69
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-05-05 15:26:49 -0400

    website project text

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "ui";
-
 export default function Test() {
   return (
     <>
-      <h1>Docs project</h1>
-      <Button>Shared Button</Button>
+      <h1>Website project</h1>
     </>
   );
 }
