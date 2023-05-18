394ba533c8a88bf95d79cfef43def8b97b6a36d8
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-05-18 14:35:28 -0400

    upload project

--- apps/old/src/shared/components/elements/Inputs/index.tsx
+++
@@ -1,4 +0,0 @@
-export { InputPassword } from './InputPassword'
-export { InputText } from './InputText'
-export { InputGroup } from './InputGroup'
-export { InputSearch } from './InputSearch'
--- apps/old/src/shared/utils/getRefValue.ts
+++
@@ -1,4 +0,0 @@
-import { MutableRefObject } from 'react'
-
-export const getRefValue = (ref: MutableRefObject<HTMLInputElement>) =>
-  ref?.current?.value
