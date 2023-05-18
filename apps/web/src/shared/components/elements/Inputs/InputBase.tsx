e0e473e2259874865b21f42f12b44ee8f843dd6c
Daniel  Hortencio <hortencio.daniel@gmail.com>
2023-05-18 15:27:47 -0400

    config10

--- apps/web/src/shared/components/elements/Inputs/InputBase.tsx
+++ apps/web/src/shared/components/elements/Inputs/InputBase.tsx
@@ -1,48 +1,50 @@
-'use client'
+"use client";
 
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
 export interface InputBaseProps {
-  name?: string
-  placeholder?: string
-  type?: 'text' | 'password' | 'number'
-  icon?: ReactNode
-  className?: string
-  value?: string
-  onChange?: (value: string) => void
-  required?: boolean
+  name?: string;
+  placeholder?: string;
+  type?: "text" | "password" | "number";
+  icon?: ReactNode;
+  className?: string;
+  value?: string;
+  onChange?: (value: string) => void;
+  required?: boolean;
 }
 
 export const InputBase = ({
   placeholder,
-  type = 'text',
+  type = "text",
   icon,
   className,
   value,
   onChange,
   name,
-  required = false
+  required = false,
 }: InputBaseProps) => {
   return (
     <div
-      className={`bg-white flex items-center h-12 justify-between rounded-full overflow-hidden relative ${className}`}
+      className={`relative flex h-12 items-center justify-between overflow-hidden rounded-full bg-white ${className}`}
     >
       <input
         name={name}
         placeholder={placeholder}
-        type={type === 'number' ? 'tel' : type}
-        className={`w-full placeholder-custom-gray-400 pl-6 pr-14 h-full text-custom-gray-500 `}
+        type={type === "number" ? "tel" : type}
+        className={`h-full w-full pl-6 pr-14 text-custom-gray-500 placeholder-custom-gray-400 `}
         value={value}
-        onChange={e =>
-          type === 'number'
-            ? onChange(e.target.value.replace(/\D/g, ''))
-            : onChange(e.target.value)
-        }
+        onChange={(e) => {
+          if (onChange) {
+            type === "number"
+              ? onChange(e.target.value.replace(/\D/g, ""))
+              : onChange(e.target.value);
+          }
+        }}
         required={required}
       />
       {icon && (
-        <div className="absolute h-full right-0 flex items-center">{icon}</div>
+        <div className="absolute right-0 flex h-full items-center">{icon}</div>
       )}
     </div>
-  )
-}
+  );
+};
